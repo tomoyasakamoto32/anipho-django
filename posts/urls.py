59cda3d62e7fa971a3a45e02934e176c4cbbc152
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-06-15 18:35:38 +0900

    create DetailView

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-  PostListView, PostCreateView
+  PostListView, PostCreateView, PostDetailView
 )
 from . import views
 
@@ -10,5 +10,5 @@ app_name = 'posts'
 urlpatterns = [
   path('post_list/', PostListView.as_view(), name='post_list'),
   path('post_create/', views.post_create, name='post_create'),
-  path('post_detail/', DetailView, name='post_create'),
+  path('post_detail/<int:pk>', PostDetailView.as_view(), name='post_detail'),
 ]
